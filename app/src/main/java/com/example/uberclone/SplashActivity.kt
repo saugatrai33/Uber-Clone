55059c72d04415dfbf82c499c05138a5db520858
Saugat <saugatrai33@gmail.com>
2023-03-10 12:47:51 +0545

    project setup

---
+++ app/src/main/java/com/example/uberclone/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.uberclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+    }
+}
